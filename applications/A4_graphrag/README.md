f70f66fe04a655d8f76cf1c5be23a38536514cfb
I747947 <tamas.ujvari@sap.com>
2026-02-12 15:28:54 +0100

    knowledge graph

---
+++ applications/A4_graphrag/README.md
@@ -0,0 +1,13 @@
+# GraphRag
+
+[langchain doc](https://docs.langchain.com/oss/python/integrations/providers/graph_rag)
+[other doc](https://datastax.github.io/graph-rag/guide/#packages-and-components)
+
+
+```
+uv init example
+cd example
+
+uv add langchain
+uv add langchain-graph-retriever
+```
--- applications/A6_knowledge_graph_pydantic/example/main.py
+++ applications/A6_knowledge_graph_pydantic/example/main.py
@@ -35,22 +35,31 @@ agent = create_agent(
 )
 
 def generate_knowledge_graph(url: str) -> knowledge_graph.KnowledgeGraph:
-    html = requests.get(url).text
+    headers = {
+        "User-Agent": f"YourAppName/1.0 ({url}; your_email@example.com)"
+    }
+    html = requests.get(url, headers=headers).text
     text = BeautifulSoup(html, "html.parser").get_text()
 
     #text = "John and Jane Doe are siblings."
-    text = """
-    Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American businessman, co-inventor, and investor. A pioneer of the personal computer revolution of the 1970s and 1980s, Jobs co-founded Apple Inc. (as Apple Computer Company) with Steve Wozniak and Ronald Wayne in 1976. After the company's board of directors fired him in 1985, he founded NeXT the same year and purchased Pixar in 1986, becoming its chairman and majority shareholder until 2007. Jobs returned to Apple in 1997 as CEO, where he was closely involved with the creation and promotion of many of the company's most influential products until his resignation in 2011.
-
-Jobs was born in San Francisco in 1955 and adopted shortly afterwards. He attended Reed College in 1972 before withdrawing that same year. In 1974, he traveled through India, seeking enlightenment before later studying Zen Buddhism. He and Wozniak co-founded Apple in 1976 to further develop and sell Wozniak's Apple I personal computer. Together, the duo gained fame and wealth a year later with production and sale of the Apple II, one of the first highly successful mass-produced microcomputers.
-
-Jobs saw the commercial potential of the Xerox Alto in 1979, which was mouse-driven and had a graphical user interface (GUI). This led to the development of the largely unsuccessful Apple Lisa in 1983, followed by the breakthrough Macintosh in 1984, the first mass-produced computer with a GUI. The Macintosh launched the desktop publishing industry in 1985 (for example, the Aldus PageMaker) with the addition of the Apple LaserWriter, the first laser printer to feature vector graphics and PostScript.
-
-In 1985, Jobs departed Apple after a long power struggle with the company's board and its then-CEO, John Sculley. That same year, Jobs took some Apple employees with him to found NeXT, a computer platform development company that specialized in computers for higher-education and business markets, serving as its CEO. In 1986, he bought the computer graphics division of Lucasfilm, which was spun off independently as Pixar.[2] Pixar produced the first computer-animated feature film, Toy Story (1995), and became a leading animation studio, producing dozens of commercially successful and critically acclaimed films.
-
-
-"""
+#    text = """
+#    Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American businessman, co-inventor, and investor. A pioneer of the personal computer revolution of the 1970s and 1980s, Jobs co-founded Apple Inc. (as Apple Computer Company) with Steve Wozniak and Ronald Wayne in 1976. After the company's board of directors fired him in 1985, he founded NeXT the same year and purchased Pixar in 1986, becoming its chairman and majority shareholder until 2007. Jobs returned to Apple in 1997 as CEO, where he was closely involved with the creation and promotion of many of the company's most influential products until his resignation in 2011.
+#
+#Jobs was born in San Francisco in 1955 and adopted shortly afterwards. He attended Reed College in 1972 before withdrawing that same year. In 1974, he traveled through India, seeking enlightenment before later studying Zen Buddhism. He and Wozniak co-founded Apple in 1976 to further develop and sell Wozniak's Apple I personal computer. Together, the duo gained fame and wealth a year later with production and sale of the Apple II, one of the first highly successful mass-produced microcomputers.
+#
+#Jobs saw the commercial potential of the Xerox Alto in 1979, which was mouse-driven and had a graphical user interface (GUI). This led to the development of the largely unsuccessful Apple Lisa in 1983, followed by the breakthrough Macintosh in 1984, the first mass-produced computer with a GUI. The Macintosh launched the desktop publishing industry in 1985 (for example, the Aldus PageMaker) with the addition of the Apple LaserWriter, the first laser printer to feature vector graphics and PostScript.
+#
+#In 1985, Jobs departed Apple after a long power struggle with the company's board and its then-CEO, John Sculley. That same year, Jobs took some Apple employees with him to found NeXT, a computer platform development company that specialized in computers for higher-education and business markets, serving as its CEO. In 1986, he bought the computer graphics division of Lucasfilm, which was spun off independently as Pixar.[2] Pixar produced the first computer-animated feature film, Toy Story (1995), and became a leading animation studio, producing dozens of commercially successful and critically acclaimed films.
+#
+#
+#"""
     
+    #print(text[2000:6000])
+
+    #make the text smaller
+    text = text[2000:3000]
+    print(text)
+    #exit(0)
 
     prompt = {
         "messages": [
@@ -66,7 +75,7 @@ In 1985, Jobs departed Apple after a long power struggle with the company's boar
     return result["structured_response"]
 
 
-kg = generate_knowledge_graph("https://en.wikipedia.org/wiki/Algorithmic_bias")
+kg = generate_knowledge_graph("https://en.wikipedia.org/wiki/Ant%C3%B3nio_Jos%C3%A9_Seguro")
 print(f"result: {kg}")
 
 vizualization.render_graph(kg)
