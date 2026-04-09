2383960dd7fc0038de5f1d9fb6f1862e2ff048a1
I747947 <tamas.ujvari@sap.com>
2026-04-09 20:03:11 +0200

    gemma4

--- applications/A2_youtube_summarizer/youtubesummarizer/main.py
+++ applications/A2_youtube_summarizer/youtubesummarizer/main.py
@@ -3,7 +3,7 @@ from langchain_ollama import ChatOllama
 
 from youtube import youtube
 
-used_model = "gemma3:4b" #"gemma3:1b"
+used_model = "gemma4:e4b" #"gemma3:4b" #"gemma3:1b"
 
 llm = ChatOllama(
     model=used_model,
@@ -11,13 +11,6 @@ llm = ChatOllama(
     # other params...
 )
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
 
 def summarize_youtube_video(url: str):
     transcript = youtube.get_transcript(url)
@@ -35,5 +28,7 @@ def summarize_youtube_video(url: str):
 
     print(ai_msg.content)
 
+
+yt_link = input("youtube link: ")
 #summarize_youtube_video("https://www.youtube.com/watch?v=eur8dUO9mvE")
-summarize_youtube_video("https://www.youtube.com/watch?v=jb4AAFCRPrI")
+summarize_youtube_video(yt_link)
